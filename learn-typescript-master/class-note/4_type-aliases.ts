2aa34f1df40505a6e1680764eab2fe49c916d0cc
yjcho <jinjanic91@gmail.com>
2021-01-14 22:58:38 +0900

    type별칭

---
+++ learn-typescript-master/class-note/4_type-aliases.ts
@@ -0,0 +1,22 @@
+type myString = string;
+const nothing: myString = 'hi';
+
+interface Human {
+    name: string;
+    age: number;
+}
+
+type Person = {
+    name: string;
+    age: number;
+}
+
+var sera: Human = {
+    name: 'sera',
+    age: 30
+}
+
+var vision: Person = {
+    name: 'vision',
+    age: 1
+}
